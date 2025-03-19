1423bda6ad674b63d37a974d23fe337e9e07766e
Panday2024 <142554187+Panday2024@users.noreply.github.com>
2025-03-20 00:30:27 +0545

    navbar

--- includes/header.php
+++ includes/header.php
@@ -10,5 +10,31 @@
     <title>Navbar</title>
 </head>
 <body>
+<nav class="navbar">
+        <div><a class="logo" href="#">eLearning</a></div>
+
+        <div class="right-icons">
+            <a href="#">Our Courses</a>
+            <a href="#">E-Book</a>
+            <a href="#">My Learning</a>
+
+            <!-- Profile Dropdown -->
+            <div class="profile-section">
+                <div>
+                    <a href="#"><i class="fa fa-sign-in"></i> LOG IN</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <nav class="navbar">
+        <div class="categories">
+            <a href="#">Development</a>
+            <a href="#">Business</a>
+            <a href="#">IT & Software</a>
+            <a href="#">Design</a>
+            <a href="#">Marketing</a>
+        </div>
+    </nav>
 </body>
 </html>
