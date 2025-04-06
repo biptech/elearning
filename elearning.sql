36f8323839c8743ccffb6fa8ea622e5212d386e3
Bipin Chapai <96807663+biptech@users.noreply.github.com>
2025-04-06 19:26:36 +0545

    sql file

---
+++ elearning.sql
@@ -0,0 +1,286 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 06, 2025 at 03:38 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `elearning`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(100) NOT NULL,
+  `user_id` int(100) NOT NULL,
+  `pid` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `price` int(10) NOT NULL,
+  `quantity` int(10) NOT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(100) NOT NULL,
+  `user_id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `number` varchar(12) NOT NULL,
+  `message` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `details` varchar(500) NOT NULL,
+  `price` int(10) NOT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `id` int(11) NOT NULL,
+  `AdminUserName` varchar(255) DEFAULT NULL,
+  `AdminPassword` varchar(255) DEFAULT NULL,
+  `AdminEmailId` varchar(255) DEFAULT NULL,
+  `userType` int(11) DEFAULT NULL,
+  `CreationDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`id`, `AdminUserName`, `AdminPassword`, `AdminEmailId`, `userType`, `CreationDate`, `UpdationDate`) VALUES
+(1, 'admin', '12f6a9d3f316650988fec54ce828762a', 'admin@gmail.com', 1, '2024-02-12 18:30:00', '2025-02-14 13:34:43');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcategory`
+--
+
+CREATE TABLE `tblcategory` (
+  `id` int(11) NOT NULL,
+  `CategoryName` varchar(200) DEFAULT NULL,
+  `Description` mediumtext DEFAULT NULL,
+  `PostingDate` timestamp NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `Is_Active` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcomments`
+--
+
+CREATE TABLE `tblcomments` (
+  `id` int(11) NOT NULL,
+  `postId` int(11) DEFAULT NULL,
+  `name` varchar(120) DEFAULT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `comment` mediumtext DEFAULT NULL,
+  `postingDate` timestamp NULL DEFAULT current_timestamp(),
+  `status` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblposts`
+--
+
+CREATE TABLE `tblposts` (
+  `id` int(11) NOT NULL,
+  `PostTitle` longtext DEFAULT NULL,
+  `CategoryId` int(11) DEFAULT NULL,
+  `SubCategoryId` int(11) DEFAULT NULL,
+  `PostDetails` longtext CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `PostingDate` timestamp NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `Is_Active` int(11) DEFAULT NULL,
+  `PostUrl` mediumtext DEFAULT NULL,
+  `PostImage` varchar(255) DEFAULT NULL,
+  `viewCounter` int(11) DEFAULT NULL,
+  `postedBy` varchar(255) DEFAULT NULL,
+  `lastUpdatedBy` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblsubcategory`
+--
+
+CREATE TABLE `tblsubcategory` (
+  `SubCategoryId` int(11) NOT NULL,
+  `CategoryId` int(11) DEFAULT NULL,
+  `Subcategory` varchar(255) DEFAULT NULL,
+  `SubCatDescription` mediumtext DEFAULT NULL,
+  `PostingDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `Is_Active` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_signup`
+--
+
+CREATE TABLE `user_signup` (
+  `u_id` int(11) NOT NULL,
+  `u_name` varchar(100) NOT NULL,
+  `u_image` varchar(100) NOT NULL,
+  `u_address` varchar(100) NOT NULL,
+  `u_email` varchar(50) NOT NULL,
+  `u_phone` varchar(15) NOT NULL,
+  `u_gender` varchar(20) NOT NULL,
+  `u_password` varchar(255) NOT NULL,
+  `u_reg_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `user_signup`
+--
+
+INSERT INTO `user_signup` (`u_id`, `u_name`, `u_image`, `u_address`, `u_email`, `u_phone`, `u_gender`, `u_password`, `u_reg_date`) VALUES
+(14, 'Mr. Bipin Chapai', '', 'Parbat', 'bipinchapai2025@gmail.com', '9811111225', 'Male', '$2y$10$MHa4Z4eyL5nET7GGDk/.cOT2YX.e7be69uYrCJAIQF84Nl9xYpC/e', '2025-04-04 08:13:41');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `AdminUserName` (`AdminUserName`);
+
+--
+-- Indexes for table `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblcomments`
+--
+ALTER TABLE `tblcomments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`),
+  ADD KEY `postId` (`postId`);
+
+--
+-- Indexes for table `tblposts`
+--
+ALTER TABLE `tblposts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`),
+  ADD KEY `postcatid` (`CategoryId`),
+  ADD KEY `postsucatid` (`SubCategoryId`),
+  ADD KEY `subadmin` (`postedBy`);
+
+--
+-- Indexes for table `tblsubcategory`
+--
+ALTER TABLE `tblsubcategory`
+  ADD PRIMARY KEY (`SubCategoryId`),
+  ADD KEY `catid` (`CategoryId`);
+
+--
+-- Indexes for table `user_signup`
+--
+ALTER TABLE `user_signup`
+  ADD PRIMARY KEY (`u_id`),
+  ADD UNIQUE KEY `u_id` (`u_id`),
+  ADD UNIQUE KEY `u_email` (`u_email`),
+  ADD UNIQUE KEY `u_phone` (`u_phone`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
+
+--
+-- AUTO_INCREMENT for table `tblposts`
+--
+ALTER TABLE `tblposts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `user_signup`
+--
+ALTER TABLE `user_signup`
+  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
