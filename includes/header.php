fec1851865002e7bdb188f65c772a252bbc200d8
Bipin Chapai <96807663+biptech@users.noreply.github.com>
2025-04-11 16:38:37 +0545

    update header.php css

--- includes/header.php
+++ includes/header.php
@@ -37,7 +37,7 @@ if (isset($_SESSION['username'])) {
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
     />
-    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/header-css.css">
 
 </head>
 <body>
